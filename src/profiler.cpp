bcd3332afd33bb357db3dcdc961fe34574dbf44a
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-05-31 13:29:19 +0300

    Remove the "Profiling started" message

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -1379,7 +1379,6 @@ Error Profiler::runInternal(Arguments& args, std::ostream& out) {
             if (error) {
                 return error;
             }
-            out << "Profiling started\n";
             break;
         }
         case ACTION_STOP: {
