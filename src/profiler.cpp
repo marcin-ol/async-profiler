0edb2379228eccd54dc2b8e93b7d0a01cf15fb01
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-08-03 22:49:44 +0300

    Enocde buildid+offset of unknown symbols in the frame (#1)

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -283,7 +283,7 @@ CodeCache* Profiler::findLibraryByAddress(const void* address) {
 
 const char* Profiler::findNativeMethod(const void* address) {
     CodeCache* lib = findLibraryByAddress(address);
-    return lib == NULL ? NULL : lib->binarySearch(address);
+    return lib == NULL ? NULL : lib->binarySearch(address, _call_trace_storage.get_allocator(), _add_build_ids);
 }
 
 bool Profiler::isAddressInCode(uintptr_t addr) {
@@ -959,6 +959,8 @@ Error Profiler::start(Arguments& args, bool reset) {
             }
         }
     }
+
+    _add_build_ids = args._build_ids;
 
     _safe_mode = args._safe_mode;
     if (VM::hotspot_version() < 8) {
